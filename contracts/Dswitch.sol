607fa96747849c583371ccfb150b805e3c695c2d
Raahul Vignesh <raahulvignesh@Raahuls-MacBook-Pro.local>
2020-06-21 18:04:11 +0530

    updated index.js and Dswitch Contract

--- contracts/Dswitch.sol
+++ contracts/Dswitch.sol
@@ -1,31 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
 
-//import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-
 contract Dswitch {
 
     address payable presetAddress;
-    bool active;
+    address public ownerAddress;
+    bool public active;
+    uint256 lastActive;
+    uint intervalTime;
+
+    fallback() external payable{}
+
+      modifier onlyOwner {
+        require(msg.sender == ownerAddress, "Only owner can call this function.");
+        _;
+    }
 
     event StillActive(
-        string status
+        string status,
+        uint256 lastActive
     );
 
-     constructor(address payable pAddress) public {
-        presetAddress = pAddress;
+    event Transfer(
+        address _from,
+        address _to,
+        uint amount
+    );
+
+     constructor(address payable _presetAddress, uint _intervalTime) public {
+        ownerAddress = msg.sender;
+        presetAddress = _presetAddress;
+        intervalTime = _intervalTime;
         active = true;
     }
 
-    function still_active() public  returns(bool) {
+    function still_active() public onlyOwner returns(bool) {
         active = true;
-        emit StillActive("User Still Alive");
+        lastActive = block.timestamp;
+        emit StillActive("User Still Alive", block.timestamp);
         return active;
     }
 
     function transfer() public payable {
-        active = false;
-        presetAddress.transfer(msg.value);
+        require(block.timestamp >= (lastActive + 200 seconds), "cannot transfer the funds, user still alive");
+        require(msg.sender == presetAddress, "Not an authorized heir");
+        presetAddress.transfer(address(this).balance);
+        emit Transfer(address(this), presetAddress, address(this).balance);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 
 }
--- script/index.js
+++ script/index.js
@@ -4,75 +4,24 @@ const url = "http://127.0.0.1:7545";
 var web3 = new Web3(new Web3.providers.HttpProvider(url));
 const publicKey = "0x253fc6c11f910918fa07c20c80c8924fa7fde4ee";
 
-const abi = [
-    {
-      "inputs": [
-        {
-          "name": "pAddress",
-          "type": "address"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "name": "status",
-          "type": "string"
-        }
-      ],
-      "name": "StillActive",
-      "type": "event"
-    },
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "still_active",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "transfer",
-      "outputs": [],
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "function"
-    }
-  ]
-const address = "0xe92f1A308661BD34050C36E569F60946E992B8BC";
-// method hardcoding the abi and contract address
-var contract = new web3.eth.Contract(abi, address);
-var res = contract.methods.still_active().call();
-console.log(res);
 
-// using truffle directly
-// const contract1 = async () => {
-//         contract1 = await dswitch.deployed();
-//         var res = contract1.methods.still_active().call();
-//         console.log(res);
-//         return res;
-//        }
+async function execute() {
+  contract1 = await dswitch.deployed();
+  var res = await contract1.still_active();
+  console.log(res);
+  return res;
+}
 
-// subscribing to events
 
-// dswitch.events.StillActive({
-//     fromBlock: latest
-// }).on("data", function(event){
-//     console.log(event.blockNumber);
-// });
+module.exports = async function (callback) {
+  try {
+    await execute()
+  } catch (e) {
+    console.log(e)
+  }
+  callback()
+}
+
 
 
 
